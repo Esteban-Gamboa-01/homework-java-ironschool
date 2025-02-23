368ea63a046a65c9637ae442ee1f0a1a1ab57b5b
alvaro <alvaro@ecample.com>
2025-02-23 21:38:27 +0100

    Added class Teacher

--- homework02/src/main/java/com/ironhack/Main.java
+++ homework02/src/main/java/com/ironhack/Main.java
@@ -1,8 +1,14 @@
 package com.ironhack;
 
 public class Main {
+    public static int global_int = 0;
+
     public static void main(String[] args) {
         //Menu
         // Testing branch maindev01
+        Teacher john = new Teacher("John",23_000);
+        Teacher jane = new Teacher("Jane",24_000);
+        System.out.println(john.getTeacherId());
+        System.out.println(jane.getTeacherId());
     }
 }
--- homework02/src/main/java/com/ironhack/Teacher.java
+++ homework02/src/main/java/com/ironhack/Teacher.java
@@ -1,4 +1,38 @@
 package com.ironhack;
 
 public class Teacher {
+    private String teacherId; //AUTO GEN
+    private String name;
+    private double salary;
+
+    public Teacher(String name, double salary) {
+        this.name = name;
+        this.salary = salary;
+        this.teacherId = "t" + Main.global_int;
+        Main.global_int++;
+    }
+
+    public String getTeacherId() {
+        return teacherId;
+    }
+
+    public void setTeacherId(String teacherId) {
+        this.teacherId = teacherId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
 }
