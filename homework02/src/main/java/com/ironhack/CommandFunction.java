0a9b38a30c379fde5df03b6c28d878ab4c79fa7a
alvaro <alvaro@ecample.com>
2025-02-25 12:48:22 +0100

    Fix folder structure

--- homework02/src/main/java/com/ironhack/CommandFunction.java
+++ homework02/src/main/java/com/ironhack/CommandFunction.java
@@ -1,4 +1,16 @@
 package com.ironhack;
 
 public class CommandFunction {
+
+    public static boolean sanitize(String args){
+        boolean sanitize = false;
+        String values[] = args.split( " ");
+        if (values.length > 3) throw new IllegalArgumentException();
+        if (values.length < 2) throw new IllegalArgumentException();
+        if (values[0].equals("ENROLL") || values[0].equals("ASSIGN") || values[0].equals("SHOW") || values[0].equals("LOOKUP")){
+            // VAMOS BIEN
+
+        }
+        return sanitize;
+    }
 }
